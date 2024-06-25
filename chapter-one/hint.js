bfa6cb75fd81cfcb60484d3aa4f70949b323070d
Salvin <chowdhuryj@msoe.edu>
2024-06-25 12:03:24 +0600

    chapter one done!

---
+++ chapter-one/hint.js
@@ -0,0 +1,10 @@
+
+
+function f(condition) {
+    switch(condition) {
+        case 1:
+            console.log(1);
+        case 2:
+            console.log(1);
+    }
+}
--- chapter-one/strict.js
+++ chapter-one/strict.js
@@ -1,2 +1,18 @@
 
+'use strict';
+
+function foo() {
+    'use strict';
+}
+
+function sloppyFunc() {
+    // sloppyVar = 123;
+} sloppyFunc(); // creates global variable `sloppyVar`
+console.log(sloppyVar);
+
+function strictFunc() {
+    'use strict';
+    // strictVar = 123;
+}
+strictFunc(); // ReferenceError: strictVar isn't defined;
 
